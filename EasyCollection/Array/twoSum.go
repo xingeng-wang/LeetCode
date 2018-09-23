2298c3710a93335b240b3b7ea7cf1dc7570ac572
Xingeng Wang <xwang@vendasta.com>
2018-09-23 17:37:15 -0600

    better way in golang for twoSum

--- EasyCollection/Array/twoSum.go
+++ EasyCollection/Array/twoSum.go
@@ -4,14 +4,13 @@ package Array
 // You may assume that each input would have exactly one solution, and you may not use the same element twice.
 // https://leetcode.com/explore/interview/card/top-interview-questions-easy/92/array/546/
 func twoSum(nums []int, target int) []int {
-	for i, a := range nums {
-		for j, b := range nums {
-			if i == j {
-				continue
-			}
-			if a+b == target {
-				return []int{i, j}
-			}
+	indexDict := make(map[int]int)
+	for i, num := range nums {
+		_, ok := indexDict[target-num]
+		if ok {
+			return []int{indexDict[target-num], i}
+		} else {
+			indexDict[num] = i
 		}
 	}
 	return []int{}
